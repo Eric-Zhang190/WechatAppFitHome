694455edb883256a811c9558252aa327271b134c
Eric-Zhang190 <109322363+Eric-Zhang190@users.noreply.github.com>
2022-07-14 21:16:22 -0400

    Initial Commit

---
+++ miniprogram/data/list-data.js
@@ -0,0 +1,50 @@
+let list_data = [
+
+  {
+    date: '2021-03-05',
+    title: 'Westchester Park',
+    detail_img: '/images/1.jpg',
+    detail_content: '此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1；此处有房源介绍-房源1',
+    love_count: 88,
+    comment_count: 66,
+    postId: 0
+  },
+  
+  {
+    date: '2021-06-08',
+    title: 'College Park',
+    detail_img: '/images/2.jpg',
+    detail_content: '此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；此处有房源介绍-房源2；',
+    love_count: 77,
+    comment_count: 77,
+    postId: 1
+  
+  },
+  
+  {
+    date: '2021-09-15',
+    title: 'Westchester Park',
+    detail_img: '/images/3.jpg',
+    detail_content: '此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；此处有房源介绍-房源3；',
+    love_count: 66,
+    comment_count: 88,
+    postId: 2
+  
+  },
+  
+  {
+    date: '2021-12-28',
+    title: '房源5',
+    detail_img: '/images/5.jpg',
+    detail_content: '此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；此处有房源介绍-房源4；',
+    love_count: 13,
+    comment_count: 14,
+    postId: 3
+  
+  }
+  
+  ];
+  
+  module.exports = {list_data};
+  
+  
---
+++ miniprogram/pages/details/detail.js
@@ -0,0 +1,144 @@
+// pages/details/detail.js
+let data = require('../../data/list-data');
+Page({
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detailObj: {},
+    index: null
+  },
+
+   /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options);
+    let index = options.index;
+    this.setData({
+      detailObj: data.list_data[index],
+      index: index
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  
+  onShareAppMessage() {
+    return {
+      title: 'form',
+      path: 'page/component/pages/form/form'
+    }
+  },
+
+  data: {
+    pickerHidden: true,
+    chosen: ''
+  },
+
+  pickerConfirm(e) {
+    this.setData({
+      pickerHidden: true
+    })
+    this.setData({
+      chosen: e.detail.value
+    })
+  },
+
+  pickerCancel() {
+    this.setData({
+      pickerHidden: true
+    })
+  },
+
+  pickerShow() {
+    this.setData({
+      pickerHidden: false
+    })
+  },
+
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
+    })
+  },
+
+  //---------------------------------------------- //
+  data: {
+    array: ['整租', '单间'],
+    objectArray: [
+      {
+        id: 0,
+        name: '整租'
+      },
+      {
+        id: 1,
+        name: '单间'
+      }
+    ],
+    index: 0
+  
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  }
+  
+
+  // ------------------------------------------//
+})
